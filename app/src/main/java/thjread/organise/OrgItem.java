8743b905cd0c0f0ca21d5cb0a97cd2c99c1017bc
Thomas Read <thjread@gmail.com>
2016-06-08 16:58:24 +0100

    Date parsing

--- app/src/main/java/thjread/organise/OrgItem.java
+++ app/src/main/java/thjread/organise/OrgItem.java
@@ -2,6 +2,8 @@ package thjread.organise;
 
 import android.util.Log;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -13,7 +15,9 @@ public class OrgItem {
     int keyword;
     Date scheduled;
     Date deadline;
-    Date done;
+    Date closed;
+
+    SimpleDateFormat format;
 
     HashMap<String, Integer> keywords;
 
@@ -22,18 +26,23 @@ public class OrgItem {
     public OrgItem(HashMap<String, Integer> keywords) {
         this.keywords = keywords;
         children = new ArrayList<OrgItem>();
+        scheduled = null;
+        deadline = null;
+        closed = null;
+        format = new SimpleDateFormat("yyyy-MM-dd");
     }
     
     public OrgItem(HashMap<String, Integer> keywords, String title, Date scheduled,
-                   Date deadline, Date done, int keyword, int treeLevel) {
+                   Date deadline, Date closed, int keyword, int treeLevel) {
         this.keywords = keywords;
         this.title = title;
         this.scheduled = scheduled;
         this.deadline = deadline;
-        this.done = done;
+        this.closed = closed;
         this.keyword = keyword;
         this.treeLevel = treeLevel;
         children = new ArrayList<OrgItem>();
+        format = new SimpleDateFormat("yyyy-MM-dd");
     }
 
     public void addChild(OrgItem child) {
@@ -97,6 +106,9 @@ public class OrgItem {
 
         title = "";
         for (; tokenPointer < tokens.length; ++tokenPointer) {
+            tokenPointer = parsePlanning(tokens, tokenPointer);
+            if (tokenPointer >= tokens.length) break;
+
             title += tokens[tokenPointer];
             if (tokenPointer != tokens.length-1) {
                 title += " ";
@@ -164,8 +176,91 @@ public class OrgItem {
         if (starNum(tokens[0]) != 0) {
             return false;
         } else {
-            section += line + "\n";
+            int tokenPointer = 0;
+            while (tokenPointer < tokens.length) {
+                tokenPointer = parsePlanning(tokens, tokenPointer);
+                if (tokenPointer >= tokens.length) break;
+
+                section += tokens[tokenPointer] + " ";
+                tokenPointer++;
+            }
+            if (section.length() > 0) {
+                section = section.substring(0, section.length() - 1) + "\n";
+            }
             return true;
         }
+    }
+
+    private int parsePlanning(String[] tokens, int tokenPointer) {
+        if (tokens.length <= tokenPointer+1) {
+            return tokenPointer;
+        }
+        Pair<Date, Integer> p = readTimestamp(tokens, tokenPointer+1);
+        if (p == null) {
+            return tokenPointer;
+        }
+        int returnPointer = p.u;
+        Date d = p.t;
+
+        switch (tokens[tokenPointer]) {
+            case "DEADLINE:":
+                deadline = d;
+                break;
+            case "SCHEDULED:":
+                scheduled = d;
+                break;
+            case "CLOSED:":
+                closed = d;
+                break;
+            default:
+                return tokenPointer;
+        }
+
+        Log.d("thjread.organise", this.title + " " + d.toString());
+
+        return returnPointer;
+    }
+
+    private Pair<Date, Integer> readTimestamp(String[] tokens, int tokenPointer) {
+        if (tokens[tokenPointer].toCharArray()[0] != '<') {//TODO inactive dates
+            return null;
+        }
+        ArrayList<String> dateTokens = new ArrayList<String>();
+        boolean success = false;
+        int finalPointer = tokenPointer;
+        for (int i=tokenPointer; (!success) && (i<tokens.length); ++i) {
+            String token = tokens[i];
+            if (i == tokenPointer) {
+                token = token.substring(1);
+            }
+            if (token.toCharArray()[token.length()-1] == '>') {
+                token = token.substring(0, token.length()-1);
+                finalPointer = i;
+                success = true;
+            }
+            dateTokens.add(token);
+        }
+        if (!success) return null;
+
+        Date d;
+        try {
+            d = format.parse(dateTokens.get(0));
+        } catch (ParseException e) {
+            return null;
+        }
+
+        //TODO times, repeats
+
+        return new Pair<Date, Integer>(d, finalPointer+1);
+    }
+}
+
+class Pair<T, U> {
+    public final T t;
+    public final U u;
+
+    public Pair(T t, U u) {
+        this.t= t;
+        this.u= u;
     }
 }
